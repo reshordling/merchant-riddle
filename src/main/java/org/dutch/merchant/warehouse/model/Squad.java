d2dea81140983dc2f510b78e4d136d76ff4df638
vasily.tsvetkov <vasily.tsvetkov@multiloginapp.com>
2020-01-30 01:38:43 +0200

    A bit of naming in Squad.java

--- src/main/java/org/dutch/merchant/warehouse/model/Squad.java
+++ src/main/java/org/dutch/merchant/warehouse/model/Squad.java
@@ -3,6 +3,8 @@ package org.dutch.merchant.warehouse.model;
 import java.util.ArrayList;
 import java.util.List;
 
+// better class name than "case" in the task description
+// as it won't clash wish reserved keywords
 public class Squad {
     public final List<List<Integer>> piles = new ArrayList<>();
     public final Integer label;
