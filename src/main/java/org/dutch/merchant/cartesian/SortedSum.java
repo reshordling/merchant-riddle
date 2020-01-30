7906962196631bb06fdb0751ebaeb4951afde6f3
vasily.tsvetkov <vasily.tsvetkov@multiloginapp.com>
2020-01-30 07:20:23 +0200

    added cartesian corpus and sorting/limiting to sum up piles

---
+++ src/main/java/org/dutch/merchant/cartesian/SortedSum.java
@@ -0,0 +1,54 @@
+package org.dutch.merchant.cartesian;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class SortedSum {
+
+    // args are sorted
+    public static IntStream limitedBy(IntStream left, IntStream right, int maxSize) {
+        int[] leftArray = left.toArray();
+
+        if (leftArray.length == 0) {
+            return IntStream.empty();
+        }
+        int[] rightArray = right.toArray();
+        if (rightArray.length == 0) {
+            return IntStream.empty();
+        }
+        int leftIdx = 0;
+        int rightIdx = 0;
+        List<Integer> merged = new ArrayList<>(maxSize);
+        while (leftIdx < leftArray.length && rightIdx < rightArray.length && merged.size() < maxSize) {
+            int sum = leftArray[leftIdx] + rightArray[rightIdx];
+            if (merged.size() == 0 || merged.get(merged.size() - 1) < sum) {
+                merged.add(sum);
+            }
+            if (leftArray[leftIdx] < rightArray[rightIdx]) {
+                leftIdx++;
+            } else {
+                rightIdx++;
+            }
+        }
+
+        while (leftIdx < leftArray.length && merged.size() < maxSize) {
+            int sum = leftArray[leftIdx] + rightArray[rightArray.length - 1];
+            if (merged.size() == 0 || merged.get(merged.size() - 1) < sum) {
+                merged.add(sum);
+            }
+            leftIdx++;
+        }
+
+        while (rightIdx < rightArray.length && merged.size() < maxSize) {
+            int sum = leftArray[leftArray.length - 1] + rightArray[rightIdx];
+            if (merged.size() == 0 || merged.get(merged.size() - 1) < sum) {
+                merged.add(sum);
+            }
+
+            rightIdx++;
+        }
+
+        return merged.stream().mapToInt(x -> x);
+    }
+}
---
+++ src/test/java/org/dutch/merchant/CartesianSortedSumTest.java
@@ -0,0 +1,56 @@
+package org.dutch.merchant;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.dutch.merchant.cartesian.SortedSum;
+import org.junit.Test;
+
+import java.util.*;
+
+import static java.util.stream.IntStream.of;
+import static org.junit.Assert.assertArrayEquals;
+
+public class CartesianSortedSumTest {
+
+    @Test
+    public void testCommutative() {
+        assertCartesian(array(1, 2), array(1, 2, 3), 5);
+        assertCartesian(array(1, 2, 3), array(1, 2), 5);
+    }
+
+    @Test
+    public void testEmpty() {
+        assertCartesian(empty(), array(1, 2, 3), 5);
+        assertCartesian(empty(), empty(), 5);
+    }
+
+    @Test
+    public void testLimit() {
+        assertCartesian(array(1, 2, 3, 4, 5), array(1, 2, 3), 1);
+    }
+
+    private static void assertCartesian(int[] left, int[] right, int limit) {
+        int[] sum = SortedSum.limitedBy(of(left), of(right), limit).toArray();
+        assertArrayEquals(dumb(left, right, limit), sum);
+    }
+
+    // direct cartesian product - sum, sort, uniq, limit
+    private static int[] dumb(int[] left, int[] right, int limit) {
+        Integer[] leftList = ArrayUtils.toObject(left);
+        Integer[] rightList = ArrayUtils.toObject(right);
+        Set<Integer> merged = new HashSet<>();
+        for (int leftInt : leftList) {
+            for (int rightInt : rightList) {
+                merged.add(leftInt + rightInt);
+            }
+        }
+        return merged.stream().sorted().limit(limit).mapToInt(i -> i).toArray();
+    }
+
+    private static int[] array(int... els) {
+        return els;
+    }
+
+    private static int[] empty() {
+        return array();
+    }
+}
